7f79de0cedd5b0de4667af80d0a807910ed09fa6
trinhdacthai <129354676+trinhdacthai@users.noreply.github.com>
2024-04-19 07:07:56 +0000

    19-4-2024

--- index.php
+++ index.php
@@ -1,27 +1,40 @@
-<?php
-
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
+<h2>Ví dụ 1:</h2>
+		<p>Văn bản HTML.</p>
 		<?php 
 		
-		sayHello('TrinhdacThai');
-			
-		phpinfo(); 
+		echo '<p>Văn bản PHP!</p>';
 			
 		?>
-	</body>
-</html>
+		<p>Văn bản khác.</p>
+
+		<h2>Ví dụ 2:</h2>
+		<?php
+
+			echo '<p>Khối dữ liệu PHP 1.</p>';
+
+		?>
+		<p>Dữ liệu HTML, <?php echo 'Dữ liệu PHP 2.'; ?></p>
+		<?php echo'<b>'; ?>
+			Một ví dụ kết hợp HTML & PHP
+		<?php echo'</b>'; ?>
+
+		<?php
+			$NgonNgu1 = "PHP";
+			$Ngonngu2 = "ASP .NET";
+			echo $NgonNgu1, "và", $Ngonngu2, "là ngôn ngữ lập trình WEBSITE";
+
+		?>
+
+
+		<?php
+			$num1 = 10;
+			$num2 = 20;
+			printf("%d+%d=%d",$num1,$num2,$num1+$num2 );
+		?>
+
+		<?php 
+			define("PI", 3.14);
+			$r=10;
+			$s= PI * pow($r ,2);
+			$p = 2 * PI * $r;
+		?>
