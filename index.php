63069118c80097b8749287c21b3c4bcebdce7441
trinhdacthai <129354676+trinhdacthai@users.noreply.github.com>
2024-04-26 06:24:45 +0000

    đăng nhập

--- index.php
+++ index.php
@@ -11,16 +11,28 @@ for ($i = 1; $i <= 200; $i++) {
 }
 ?>
 
-<h2><span style="color :blue ">Đăng nhập hệ thống</span></h2>
+	<h2><span style="color :blue ">Đăng nhập hệ thống</span></h2>
 
-    <form action="dangnhap.php" >
-        <label for="username">Tên đăng nhập:</label>
-        <input type="text" id="username" name="username"><br>
-        <label for="password ">Mật khẩu:</label>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Đăng nhập ">
-    </form>
-
-
+    <form action="login" method="POST">
+		<span>Username <input type="text" name="user" size="15"></span><br>
+		<span>Password <input type=" password " name="pass" size="15"></span>
+		<br><input type="submit" name="submit" value="Đăng nhập">
+		</form>
+		<?php
+			if($_SERVER["REQUEST_METHOD"] == "POST");
+			{
+				$username = $_POST ['user'];
+				$password = $_POST ['pass'];
+				if($username == "admin" && $password == "11111")
+				{
+					echo '<b style = "color : red"> '. $username . '</b>';
+				}
+				else
+				{
+					echo 'username hoặc password không chính xác';
+				}
+			}
+		?>
+	
 	</body>
 </html>
